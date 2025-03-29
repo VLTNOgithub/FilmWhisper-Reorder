30dc51930fc2450b030fefc8c1cc9e243431ce7b
vltno <gfree6311@gmail.com>
2025-03-29 15:32:29 +0000

    e

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+	async fetch(request) {
+	  /**
+	   * Example someHost is set up to take in a JSON request
+	   * Replace url with the host you wish to send requests to
+	   * @param {string} url the URL to send the request to
+	   * @param {BodyInit} body the JSON data to send in the request
+	   */
+	  const someHost = "https://examples.cloudflareworkers.com/demos";
+	  const url = someHost + "/requests/json";
+	  const body = {
+		results: ["default data to send"],
+		errors: null,
+		msg: "I sent this to the fetch",
+	  };
+  
+	  /**
+	   * gatherResponse awaits and returns a response body as a string.
+	   * Use await gatherResponse(..) in an async function to get the response body
+	   * @param {Response} response
+	   */
+	  async function gatherResponse(response) {
+		const { headers } = response;
+		const contentType = headers.get("content-type") || "";
+		if (contentType.includes("application/json")) {
+		  return JSON.stringify(await response.json());
+		} else if (contentType.includes("application/text")) {
+		  return response.text();
+		} else if (contentType.includes("text/html")) {
+		  return response.text();
+		} else {
+		  return response.text();
+		}
+	  }
+  
+	  const init = {
+		body: JSON.stringify(body),
+		method: "POST",
+		headers: {
+		  "content-type": "application/json;charset=UTF-8",
+		},
+	  };
+	  const response = await fetch(url, init);
+	  const results = await gatherResponse(response);
+	  return new Response(results, init);
 	},
-};
+  };
